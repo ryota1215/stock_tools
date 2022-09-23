bbe85035607991c414c59b0b6dfdb87c92d40eca
ryota1215 <smallalphataker@gmail.com>
2022-09-24 00:02:51 +0900

    first_commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="stock_tools",
+    author=" ",
+    author_email="smallalphataker@gmail.com",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.6,<4.0",
+    setup_requires=["setuptools_scm"],
+    use_scm_version={"version_scheme": "python-simplified-semver"},
+)
